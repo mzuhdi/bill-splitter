165fd82e50fa7e928d583bba2a7ca53655686b5a
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2022-06-12 12:14:34 -0400

    added event listener and bit of happy path

--- js/app.js
+++ js/app.js
@@ -1,23 +1,53 @@
 const bill = document.querySelector("#bill");
-const tipSelections = document.querySelector(".tip__button__section").children;
+// const tipSelections = Array.from(
+//   document.querySelector(".tip__button__section").children
+// );
+const tipSelectionButtons = document.querySelectorAll(".tip__button");
 const numOfPeople = document.querySelector("#people");
 const tipPerPerson = document.querySelector("#tip__per__person");
 const totalPerPerson = document.querySelector("#total__per__person");
 
-let billValue = 100.0;
-let tipPercentageValue = 10;
-let numOfPeopleValue = 2;
+let billValue = 0.0;
+let tipPercentageValue = 0;
+let numOfPeopleValue = 1;
 let totalTipPerPersonValue = 0.0;
 let totalPerPersonValue = 0.0;
 
+bill.addEventListener("input", (e) => {
+  console.log("bill " + e.target.value);
+  billValue = e.target.value;
+  console.log(billValue);
+});
+
+tipSelectionButtons.forEach((tipButton) => {
+  tipButton.addEventListener("click", (e) => {
+    tipPercentageValue = e.target.value;
+    console.log(tipPercentageValue);
+    calc();
+  });
+});
+
+numOfPeople.addEventListener("input", (e) => {
+  console.log("people " + e.target.value);
+  numOfPeopleValue = e.target.value;
+  console.log(numOfPeopleValue);
+});
+
 function calc() {
-  let tip = billValue * (tipPercentageValue / 100);
-  let total = billValue + tip;
+  let tip = parseFloat(billValue * (tipPercentageValue / 100));
+  let total = parseFloat(billValue) + parseFloat(tip);
 
   totalTipPerPersonValue = tip / numOfPeopleValue;
   totalPerPersonValue = total / numOfPeopleValue;
 
   console.log(totalTipPerPersonValue);
   console.log(totalPerPersonValue);
+
+  totalPerPerson.value = "$" + totalPerPersonValue.toFixed(2);
 }
-calc();
+
+/**
+ * The total per person should get updated as
+ * when you are finish inputting bill + tip
+ *
+ */
