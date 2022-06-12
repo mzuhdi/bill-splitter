e6eee336110254e203a4cd576a995dc90477165d
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2022-06-12 13:37:47 -0400

    happy path complete and changed the tip selection

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const bill = document.querySelector("#bill");
 //   document.querySelector(".tip__button__section").children
 // );
 const tipSelectionButtons = document.querySelectorAll(".tip__button");
+const tipSelectionCustom = document.querySelector("#tip");
 const numOfPeople = document.querySelector("#people");
 const tipPerPerson = document.querySelector("#tip__per__person");
 const totalPerPerson = document.querySelector("#total__per__person");
@@ -17,6 +18,7 @@ bill.addEventListener("input", (e) => {
   console.log("bill " + e.target.value);
   billValue = e.target.value;
   console.log(billValue);
+  calc();
 });
 
 tipSelectionButtons.forEach((tipButton) => {
@@ -27,10 +29,17 @@ tipSelectionButtons.forEach((tipButton) => {
   });
 });
 
+tipSelectionCustom.addEventListener("input", (e) => {
+  tipPercentageValue = e.target.value;
+  console.log(tipPercentageValue);
+  calc();
+});
+
 numOfPeople.addEventListener("input", (e) => {
   console.log("people " + e.target.value);
   numOfPeopleValue = e.target.value;
   console.log(numOfPeopleValue);
+  calc();
 });
 
 function calc() {
@@ -43,11 +52,13 @@ function calc() {
   console.log(totalTipPerPersonValue);
   console.log(totalPerPersonValue);
 
+  tipPerPerson.value = "$" + totalTipPerPersonValue.toFixed(2);
   totalPerPerson.value = "$" + totalPerPersonValue.toFixed(2);
 }
 
 /**
- * The total per person should get updated as
- * when you are finish inputting bill + tip
- *
+ * TODO:
+ * when num of ppl is 0, the result turn into infinity
+ * active states
+ * reset button
  */
